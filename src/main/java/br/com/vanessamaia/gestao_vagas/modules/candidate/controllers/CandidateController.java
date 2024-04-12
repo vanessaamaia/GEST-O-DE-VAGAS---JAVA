82b1aeb672e138ccbe0df1bd16ea7445075a4167
VanessaMaia26 <vanessamaia2607@gmail.com>
2024-04-12 16:00:18 -0300

    structure

---
+++ src/main/java/br/com/vanessamaia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,10 @@
+package br.com.vanessamaia.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CandidateController {
+    
+}
---
+++ src/test/java/br/com/vanessamaia/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vanessamaia.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
